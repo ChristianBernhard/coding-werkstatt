04e5133adceed951f9079e7a783f6b1095b5aced
ChristianBernhard <christian.bernhard97@web.de>
2023-11-15 14:28:21 +0100

    removed links to other website again

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -82,7 +82,7 @@ function MobileNavigation() {
             <MobileNavLink href="#">Über uns</MobileNavLink>
             <MobileNavLink href="#pricing">Preise</MobileNavLink>
             <MobileNavLink href="#get-started-today">Kontakt</MobileNavLink>
-            <MobileNavLink href="https://www.ainleuchtend.de/" target="_blank" rel="noopener noreferrer">Ainleuchtend</MobileNavLink>
+            {/*<MobileNavLink href="https://www.ainleuchtend.de/" target="_blank" rel="noopener noreferrer">Ainleuchtend</MobileNavLink>*/}
             {/*<MobileNavLink href="/login">Sign in</MobileNavLink>*/}
           </Popover.Panel>
         </Transition.Child>
@@ -104,7 +104,7 @@ export function Header() {
               <NavLink href="#">Über uns</NavLink>
               <NavLink href="#pricing">Preise</NavLink>
               <NavLink href="#get-started-today">Kontakt</NavLink>
-              <NavLink href="https://www.ainleuchtend.de/" target="_blank" rel="noopener noreferrer">AInleuchtend</NavLink>
+              {/*<NavLink href="https://www.ainleuchtend.de/" target="_blank" rel="noopener noreferrer">AInleuchtend</NavLink>*/}
             </div>
           </div>
           <div className="flex items-center gap-x-5 md:gap-x-8">
